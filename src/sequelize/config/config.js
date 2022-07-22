14b17d5a2c9f3aa50e00021daac43a30bfa8de4d
Felipe Rangel <feliperangel277@gmail.com>
2022-07-22 17:38:56 -0300

    Conecta com o banco de dados

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.PASSWORD_POSTGRES,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'postgres',
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.PASSWORD_POSTGRES,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.PASSWORD_POSTGRES,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'postgres',
   },
 };
